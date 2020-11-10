8e7e7353f2be6738bc0570071853ff46540f5e27
oscar <oscaroceguera@gmail.com>
2020-11-09 23:00:15 -0700

    Addeed TS

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,31 @@
-import express from 'express'
+import express, {Request, Response, Express} from 'express'
 import fetch from 'node-fetch'
 import cookieSession from 'cookie-session'
-const app = express()
+
+const app: Express = express()
+
+const PORT: string | number = process.env.PORT || 4000
 
 const client_id = process.env.GITHUB_CLIENT_ID
-const client_secret = process.env.GITHUB_CLIENT_SECRET
+const client_secret= process.env.GITHUB_CLIENT_SECRET
 const cookie_secreet = process.env.COOKIE_SECRET
 
 app.use(cookieSession({
   secret: cookie_secreet
 }))
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('simple')
 })
 
-app.get('/login/github', (req, res) => {
-  const redirect_uri = "http://localhost:4000/login/github/callback";
+app.get('/login/github', (req: Request, res: Response): void => {
+  const redirect_uri: string = "http://localhost:4000/login/github/callback";
   res.redirect(
     `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}`
   );
 })
 
-async function getAccessToken(code) {
+async function getAccessToken(code: string): Promise<any> {
   const response = await fetch("https://github.com/login/oauth/access_token", {
     method: 'POST',
     headers: {
@@ -40,7 +43,7 @@ async function getAccessToken(code) {
   return params.get('access_token')
 }
 
-async function fetchGitHubUser(token) {
+async function fetchGitHubUser(token: string) {
   const request = await fetch("https://api.github.com/user", {
     headers: {
       Authorization: "token " + token
@@ -49,15 +52,16 @@ async function fetchGitHubUser(token) {
   return await request.json();
 }
 
-app.get('/login/github/callback', async (req, res) => {
-  const code = req.query.code
+
+app.get('/login/github/callback', async (req: Request, res: Response): Promise<any> => {
+  const code: string = req.query.code as string;
   const token = await getAccessToken(code)
   const githubData = await fetchGitHubUser(token)
   console.log('githubData', githubData)
 
   if (githubData) {
-    req.session.githubId = githubData.id
-    req.session.token = token
+    req!.session!.githubId = githubData.id
+    req!.session!.token = token
     res.redirect('/admin')
   } else {
     console.log('EERROR')
@@ -65,7 +69,7 @@ app.get('/login/github/callback', async (req, res) => {
   }
 })
 
-app.get('/admin', (req, res) => {
+app.get('/admin', (req: Request, res: Response) => {
   if (req.session && req.session.githubId === 5040939) {
     res.send('HOla oscar <pre>' + JSON.stringify(req.session, null, 2))
   } else {
@@ -73,12 +77,12 @@ app.get('/admin', (req, res) => {
   }
 })
 
-app.get('/logout', (req, res) => {
+app.get('/logout', (req: Request, res: Response) => {
   if (req.session) req.session = null
   res.redirect('/')
 })
 
-const PORT = process.env.PORT || 4000
+
 app.listen(PORT, () => {
   console.log('Listening at port:', PORT)
 })
